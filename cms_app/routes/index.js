95c392ea53986335dd4e6c31c67bcb37e02caeea
BL MacBook <BLMac@dhcp-v042-121.mobile.uci.edu>
2018-01-17 13:28:31 -0800

    Adds registered users into database and redirects

--- cms_app/routes/index.js
+++ cms_app/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
+var userSchema = mongoose.Schema({ 
+		email: String,
+		password: String
+});
+
+var userModel= mongoose.model('users', userSchema);
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res){
+		var newUser = new userModel({
+			email: 'thi@thi.com', 
+			password: "blah", 
+		});
+		newUser.save(function(err, user){
+			if(err) return console.error(err);
+			console.log(user);
+		});
+		res.end();
+
+});
+
+router.post('/auth/register', function(req, res){
+	var newUser = new userModel({
+		email : req.body.email,
+		password: req.body.password,
+	});
+	newUser.save(function(err, user){
+		if(err) return console.error(err);
+		res.redirect('/admin');
+	});
+
 });
 
 module.exports = router;
