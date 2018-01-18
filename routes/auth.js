489c6a52e1032ec931bc6630f9794dfb87924a97
BL MacBook <BLMac@dhcp-v076-183.mobile.uci.edu>
2018-01-18 11:16:34 -0800

    login flow in auth.js; verified but does not redirect

--- routes/auth.js
+++ routes/auth.js
@@ -18,12 +18,25 @@ router.post('/register', function(req, res){
 		email : req.body.email,
 		password: req.body.password,
 	});
-	
+
 	newUser.save(function(err, user){
 		if(err) return console.error(err);
 		res.redirect('/admin');
 	});
 
+});
+
+router.post('/login', function(req, res){
+	// userModel.find({'email': req.body.email, 'password': req.body.email}, function(err, user){
+		userModel.where('email').equals(req.body.email).where('password').equals(req.body.password).exec(function(err, user){
+		if (err) return console.error(err);
+		if (user.length == 0){
+			console.log(user);
+			// res.render('auth');	
+		}
+		console.log(user);
+		res.redirect('/admin');
+	});
 });
 
 module.exports = router;
