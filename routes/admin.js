adfbb7b9a7f44edd30a753efa7b572dd6219d406
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2018-01-23 21:03:31 -0800

    Connect edit page button to form; updates pages with new content

--- routes/admin.js
+++ routes/admin.js
@@ -19,27 +19,79 @@ router.get('/', function(req, res) {
 });
 });
 
+//add new page button gets here; blank form
 router.get('/editPage', function(req, res) {
-  res.render('editPage');
+  res.render('editPage', {
+    title: "",
+    section_title: "",
+    body: "",
+    url: "",
+    footer: "",
+    template: ""
+  });
 });
 
-router.post('/addPage', function(req, res) {
-  	var newPage = new pageModel({
-		title : req.body.title,
+//edit existing page 
+router.get('/editPage/:url', function(req, res){
+  pageModel.findOne({
+    "user._id": req.user._id,
+    url: req.params.url
+  }, function(err, page){
+    if (err) return res.send(err);
+    if (page){
+        res.render('editPage', {
+          title: page.title,
+          section_title: page.section_title,
+          body: page.body,
+          url: page.url,
+          footer: page.footer,
+          template: page.template
+        });
+    };
+  });
+});
+
+router.post('/addPage/:oldUrl', function(req, res) {
+  if (req.params.oldUrl != "newPage"){
+    //find page and update it
+    pageModel.findOneAndUpdate(
+      {"user._id": req.user._id, url: req.params.oldUrl},
+      {$set: {
+        title : req.body.title,
+        section_title: req.body.section1_title,
+        body: req.body.section1_body,
+      	url: req.body.url,
+        footer: req.body.footer,
+        template: req.body.template,
+        user: req.user,
+        date: new Date(),
+        visible: true}},
+      function(err, page){
+        if(err) return console.error(err);
+        res.redirect('/admin');
+      }
+    );
+  }else{
+
+    //add new page to database
+    var newPage = new pageModel({
+  	title : req.body.title,
     section_title: req.body.section1_title,
     body: req.body.section1_body,
-		url: req.body.url,
+  	url: req.body.url,
     footer: req.body.footer,
     template: req.body.template,
     user: req.user,
     date: new Date(),
     visible: true
-	});
+    });
 
-	newPage.save(function(err, user){
-		if(err) return console.error(err);
-		res.redirect('/admin');
-	});
+    newPage.save(function(err, user){
+    	if(err) return console.error(err);
+    	res.redirect('/admin');
+    });
+  }
+
 });
 
 router.get('/editAccount', function(req, res){
