74bbc18859d7d74b78134b3ea92ca03e7c2c7dc0
BL MacBook <BLMac@dhcp-v059-053.mobile.uci.edu>
2018-02-21 13:53:40 -0800

    portal test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("CONNECTED TO MONGO");
+  console.log("CONNECTED TO MONGO; PORT 4000");
 });
 
 // client-sessions
